e94a143a21312990b98713b46c42c4dc7554c22b
Cristhian Almeida <3correntes@gmail.com>
2024-01-08 19:51:51 -0400

    Update Comment.jsx

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -5,7 +5,11 @@ const Comment = ({ createdAt, username, profilePic, text }) => {
     <Flex w={'full'} gap={4}>
       <Avatar src={profilePic} name={username} size={'sm'} />
       <Flex direction={'column'}>
-        <Flex w={'full'} alignItems={'center'} gap={2}>
+        <Flex
+          w={'full'}
+          alignItems={'center'}
+          justifyContent={'center'}
+          gap={2}>
           <Text fontWeight={'bold'} fontSize={14}>
             {username}
           </Text>
