65c416694175486f31996fd13ebd42c628fdcc52
Cristhian Almeida <3correntes@gmail.com>
2024-01-09 20:27:00 -0400

    add: googleauth

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -32,7 +32,7 @@ const AuthForm = () => {
             <Text mx={1}>OU</Text>
             <Box flex={2} h={'1px'} bg={'gray.400'} />
           </Flex>
-          <GoogleAuth />
+          <GoogleAuth prefix={isLogin ? 'Entrar' : 'Cadastrar'} />
         </VStack>
       </Box>
       <Box
