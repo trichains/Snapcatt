1c368d57c36cab7f9ed87fadfed8fc00b3d5aad9
Cristhian Almeida <3correntes@gmail.com>
2024-01-18 16:54:07 -0400

    Update AuthPage.jsx

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -3,17 +3,12 @@ import AuthForm from '../../components/AuthForm/AuthForm';
 
 const AuthPage = () => {
   return (
-    <Flex minH={'100vh'} justifyContent={'center'} alignItems={'center'} px={4}>
+    <Flex minH={{ base: '95vh', md: '100vh' }} justifyContent={'center'} alignItems={'center'} px={1}>
       <Container maxW={'container.md'} padding={0}>
         <Flex justifyContent={'center'} alignItems={'center'} gap={10}>
           {/* Lado esquerdo */}
           <Box display={{ base: 'none', md: 'block' }}>
-            <Image
-              src="/auth.webp"
-              h={'100%'}
-              w={'100%'}
-              alt="Gatinhos no celular"
-            />
+            <Image src="/auth.webp" h={'100%'} w={'100%'} alt="Gatinhos no celular" />
           </Box>
 
           {/* Lado direito */}
